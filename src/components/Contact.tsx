ec2fe805a85abe718e225156d14048948e33dc5a
gabriel-suela <gscsuela@gmail.com>
2023-03-04 19:27:55 -0300

    refactor: contact button

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -31,7 +31,7 @@ export default function Contact() {
           />
           <button
             type="button"
-            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
+            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md  bg-orange-300 text-stone-900"
           >
             Entre em contato
           </button>
